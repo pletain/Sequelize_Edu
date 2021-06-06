0a8bceb0ffee5ebf3218e55780094902b7824025
김태형 <thk12378905@gmail.com>
2021-06-06 11:19:07 +0000

    create

--- sequelize/controllers/admin/admin.ctrl.js
+++ sequelize/controllers/admin/admin.ctrl.js
@@ -1,13 +1,18 @@
+const models = require('../../models');
+
 exports.get_products = ( _ , res) => {
     res.render( 'admin/products.html' , 
         { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
     );
 }
 
-exports.get_products_write = ( _ , res) => {
-    res.render( 'admin/write.html');
-}
-
 exports.post_products_write = ( req , res ) => {
-    res.send(req.body);
+    models.Products.create({ 
+        name : req.body.name, //body-parser로 날아온 name 정보가 들어감
+        price : req.body.price , //body-parser로 날아온 price 정보가 들어감
+        description : req.body.description //body-parser로 날아온 description 정보가 들어감
+    }).then( () => { //callback 함수 처리
+        res.redirect('/admin/products');
+    });
+	
 }
